c5c17ede9a96e61c66e776dbfc2664f2a9d982a4
Yauheni Filipovich <yauheni_filipovich@epam.com>
2020-01-06 00:37:10 +0300

    add sql scripts

---
+++ patient/sql/create_table.sql
@@ -0,0 +1,24 @@
+create table patients (
+	id serial,
+	first_name varchar(16),
+	last_name varchar(16),
+	middle_name varchar(16),
+	phone_number int,
+	date_of_birth date,
+	constraint pk_patient primary key (id)
+);
+create table illness (
+	id serial,
+	name varchar(30),
+	name_in_latin varchar(30),
+    chance_to_die int CHECK ( chance_to_die BETWEEN 0 AND 100),
+	constraint pk_illness primary key (id)
+);
+create table patients_illness (
+	id serial,
+	patient_id int,
+	illness_id int,
+	constraint FK_patient foreign key(patient_id) references patients(id) on delete cascade,
+	constraint FK_illness foreign key(illness_id) references illness(id) on delete cascade,
+	constraint pk_patient_illness primary key (id)
+)
---
+++ patient/sql/fill_table.sql
@@ -0,0 +1,5 @@
+INSERT INTO patients(first_name, last_name, middle_name, phone_number,
+                     date_of_birth)
+VALUES ('Yauheni', 'Filipovich',
+        'Aleksandrovich', 3221114,
+        '2000-08-27')
