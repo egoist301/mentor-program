1c8f2a551a66f21c2847b72466ee60e0aa249a9f
Yauheni Filipovich <yauheni_filipovich@epam.com>
2020-01-14 17:08:55 +0300

    add connection pool

--- patient/src/main/java/com/epam/esm/config/ConnectionConfig.java
+++ patient/src/main/java/com/epam/esm/config/ConnectionConfig.java
@@ -1,12 +1,13 @@
 package com.epam.esm.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
@@ -15,19 +16,19 @@ import javax.sql.DataSource;
 public class ConnectionConfig {
     @Autowired
     private Environment environment;
-    private static final String DRIVER = "db.driver";
+    private static final String DRIVER = "dataSourceClassName";
     private static final String USERNAME = "db.user";
     private static final String PASSWORD = "db.pass";
     private static final String URL = "db.url";
 
     @Bean
     public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(environment.getProperty(DRIVER));
-        dataSource.setUrl(environment.getProperty(URL));
-        dataSource.setUsername(environment.getProperty(USERNAME));
-        dataSource.setPassword(environment.getProperty(PASSWORD));
-        return dataSource;
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setDriverClassName(environment.getProperty(DRIVER));
+        hikariConfig.setJdbcUrl(environment.getProperty(URL));
+        hikariConfig.setUsername(environment.getProperty(USERNAME));
+        hikariConfig.setPassword(environment.getProperty(PASSWORD));
+        return new HikariDataSource(hikariConfig);
     }
 
     @Bean
