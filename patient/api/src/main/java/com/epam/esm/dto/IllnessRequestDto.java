01b386f42dc3abdc16764c828e5e2fdc04b2fea1
Yauheni Filipovich <yauheni_filipovich@epam.com>
2020-01-22 21:05:30 +0300

    put not null on not blank

--- patient/api/src/main/java/com/epam/esm/dto/IllnessRequestDto.java
+++ patient/api/src/main/java/com/epam/esm/dto/IllnessRequestDto.java
@@ -4,21 +4,21 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 public class IllnessRequestDto {
-    @NotNull(message = "name can't be null")
+    @NotBlank(message = "name can't be null")
     @Size(min = 4, max = 30, message = "name can be 4 to 30 characters long")
     @JsonProperty("name")
     private String name;
 
-    @NotNull(message = "description can't be null")
+    @NotBlank(message = "description can't be null")
     @Size(min = 4, max = 250, message = "description can be 4 to 250 characters long")
     @JsonProperty("description")
     private String description;
 
-    @NotNull(message = "chance to die can't be null")
+    @NotBlank(message = "chance to die can't be null")
     @Min(value = 0, message = "minimum 0%")
     @Max(value = 100, message = "maximum 100%")
     @JsonProperty("chance_to_die")
--- patient/api/src/main/java/com/epam/esm/dto/PatientRequestDto.java
+++ patient/api/src/main/java/com/epam/esm/dto/PatientRequestDto.java
@@ -10,35 +10,35 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import javax.validation.Valid;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.util.Set;
 
 public class PatientRequestDto {
-    @NotNull(message = "first name can't be null")
+    @NotBlank(message = "first name can't be null")
     @Size(min = 2, max = 16, message = "first name can be 2 to 16 characters long")
     @JsonProperty("first_name")
     private String firstName;
 
-    @NotNull(message = "last name can't be null")
+    @NotBlank(message = "last name can't be null")
     @Size(min = 2, max = 16, message = "last name can be 2 to 16 characters long")
     @JsonProperty("last_name")
     private String lastName;
 
-    @NotNull(message = "middle name can't be null")
+    @NotBlank(message = "middle name can't be null")
     @Size(min = 4, max = 16, message = "middle name can be 4 to 16 characters long")
     @JsonProperty("middle_name")
     private String middleName;
 
-    @NotNull(message = "phone number can't be null")
+    @NotBlank(message = "phone number can't be null")
     @Min(value = 1000000, message = "phone number must be 7 digits long")
     @Max(value = 9999999, message = "phone number must be 7 digits long")
     @JsonProperty("phone_number")
     private Integer phoneNumber;
 
-    @NotNull(message = "date of birth can't be null")
+    @NotBlank(message = "date of birth can't be null")
     @JsonFormat(pattern = "yyyy-MM-dd")
     @JsonDeserialize(using = LocalDateDeserializer.class)
     @JsonSerialize(using = LocalDateSerializer.class)
