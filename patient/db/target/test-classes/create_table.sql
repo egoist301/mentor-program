a851d87a89349582f2ef121b896b377b141c2bfa
Evgeniy Filippovich <filippovich.z@mail.ru>
2020-02-04 15:14:20 +0300

    delete project

---
+++ patient/db/target/test-classes/create_table.sql
@@ -0,0 +1,32 @@
+create table patient
+(
+    id                    serial,
+    first_name            varchar(20)      not null,
+    last_name             varchar(20)      not null,
+    middle_name           varchar(20)      not null,
+    phone_number          int              not null,
+    date_of_birth         date             not null,
+    identification_number nchar(14) UNIQUE not null,
+    create_date           date             not null,
+    update_date           date             not null,
+    constraint pk_patient primary key (id)
+);
+create table illness
+(
+    id            serial,
+    name          varchar(30) UNIQUE                          not null,
+    description   varchar(250)                                not null,
+    chance_to_die int CHECK (chance_to_die BETWEEN 0 AND 100) not null,
+    create_date   date                                        not null,
+    update_date   date                                        not null,
+    constraint pk_illness primary key (id)
+);
+create table patient_illness
+(
+    id         serial,
+    patient_id int not null,
+    illness_id int not null,
+    constraint FK_patient foreign key (patient_id) references patient (id) on delete cascade,
+    constraint FK_illness foreign key (illness_id) references illness (id) on delete cascade,
+    constraint pk_patient_illness primary key (id)
+)
---
+++ patient/db/target/test-classes/fill_table.sql
@@ -0,0 +1,15 @@
+insert into patient( first_name, last_name, middle_name, phone_number
+                   , date_of_birth, identification_number, create_date
+                   , update_date)
+values ('Yauheni', 'Filipovich', 'Aleksandrovich', 3221114, '2020-01-17',
+        '1234567890qwer', '2020-01-17', '2020-01-17'),
+       ('first1', 'last1', 'middle1', 7654321, '2020-01-17', '0987654321rewq',
+        '2020-01-17', '2020-01-17');
+
+insert into illness(name, description, chance_to_die, create_date, update_date)
+values ('сифилис', 'desc1', 80, '2020-01-17', '2020-01-17'),
+       ('аппендицит', 'desc2', 14, '2020-01-17', '2020-01-17');
+
+insert into patient_illness(patient_id, illness_id)
+values (1, 2),
+       (1, 1);
